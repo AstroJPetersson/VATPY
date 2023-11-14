47df65227bb7fe4fb98383a775638d8cf93461b8
Jonathan Petersson <astrojpetersson@gmail.com>
2023-11-14 11:46:48 +0100

    Clean up

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-from .tplot import TerminalPlot
-from .read import read_hdf5, read_dump
-from .frame import number_density, temperature
-from .frame import density_frame, temperature_frame, velocity_frame
-
-
